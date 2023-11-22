ca619d16b6deb749db059438fd497944790d55c6
TeamDman <TeamDman9201@gmail.com>
2023-11-22 16:57:28 -0500

    add out-of-window dragging support

--- src/click_drag_movement_plugin.rs
+++ src/click_drag_movement_plugin.rs
@@ -1,4 +1,4 @@
-use bevy::input::mouse::MouseButtonInput;
+use bevy::input::mouse::{MouseButtonInput, MouseMotion};
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
@@ -16,10 +16,7 @@ impl Plugin for ClickDragMovementPlugin {
                 .in_set(MovementSet::Input)
                 .after(camera_zoom_tick),
         )
-        .insert_resource(MouseDragState {
-            start_state: None,
-            is_dragging: false,
-        });
+        .insert_resource(MouseDragState::default());
     }
 }
 
@@ -32,11 +29,23 @@ struct StartState {
 struct MouseDragState {
     start_state: Option<StartState>,
     is_dragging: bool,
+    last_known_screen_position: Option<Vec2>,
+}
+
+impl Default for MouseDragState {
+    fn default() -> Self {
+        Self {
+            start_state: None,
+            is_dragging: false,
+            last_known_screen_position: None,
+        }
+    }
 }
 
 fn update_drag(
     mut mouse_button_input_events: EventReader<MouseButtonInput>,
     mut mouse_drag_state: ResMut<MouseDragState>,
+    mut mouse_motion_events: EventReader<MouseMotion>,
     mut character: Query<&mut Transform, With<Character>>,
     window_query: Query<&Window, With<PrimaryWindow>>,
     camera_query: Query<(&Camera, &GlobalTransform), (With<MainCamera>, Without<Character>)>,
@@ -49,6 +58,7 @@ fn update_drag(
             MouseButton::Left => {
                 mouse_drag_state.is_dragging = event.state.is_pressed();
                 if mouse_drag_state.is_dragging {
+                    // begin dragging
                     if let Some(screen_position) = window.cursor_position() {
                         if let Some(world_position) = camera
                             .viewport_to_world(camera_transform, screen_position)
@@ -58,10 +68,13 @@ fn update_drag(
                                 drag_start_screen_position: screen_position,
                                 drag_start_world_position: world_position,
                             });
+                            mouse_drag_state.last_known_screen_position = Some(screen_position);
                         }
                     }
                 } else {
+                    // finish dragging
                     mouse_drag_state.start_state = None;
+                    mouse_drag_state.last_known_screen_position = None;
                 }
             }
             _ => {}
@@ -69,6 +82,7 @@ fn update_drag(
     }
 
     if mouse_drag_state.is_dragging {
+        // perform drag update
         if let Some(start_state) = &mouse_drag_state.start_state {
             if let Some(current_screen_position) = window.cursor_position() {
                 // we want the starting point to remain under the mouse as we drag
@@ -84,6 +98,16 @@ fn update_drag(
                         drag_start_screen_position: current_screen_position,
                         drag_start_world_position: start_state.drag_start_world_position,
                     });
+                    mouse_drag_state.last_known_screen_position = Some(current_screen_position);
+                }
+            } else {
+                // cursor is outside the window, use delta to approximate mouse position
+                let mut delta = mouse_motion_events.read().fold(Vec2::ZERO, |acc, event| {
+                    acc + event.delta
+                });
+                delta.x *= -1.0;
+                for mut transform in character.iter_mut() {
+                    transform.translation += delta.extend(0.0);
                 }
             }
         }
