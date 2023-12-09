3138f59a00f2f41ac129f8a832a5f3e242f615d4
TeamDman <TeamDman9201@gmail.com>
2023-12-08 19:40:26 -0500

    launches

--- src/process_communication.rs
+++ src/process_communication.rs
@@ -15,10 +15,11 @@ impl Plugin for ProcessCommunicationPlugin {
             .register_type::<MyDebugText>()
             .add_event::<StreamEvent>()
             .add_systems(Startup, setup)
-            .add_systems(
-                Update,
-                (handle_activation, read_stream, spawn_text, move_text),
-            );
+            // .add_systems(
+            //     Update,
+            //     (handle_activation),
+            // );
+            ;
     }
 }
 
@@ -65,84 +66,80 @@ fn setup(mut commands: Commands) {
         Collider::cuboid(100.0, 100.0), // make the interaction range larger than the button itself
         Name::new("SpawnProcessPressurePlate"),
     ));
-}
 
-fn handle_activation(
-    mut commands: Commands,
-    mut reader: EventReader<PressurePlateActivationEvent>,
-    query: Query<&HoverShowerPressurePlate>,
-) {
-    for event in reader.read() {
-        if let Ok(plate) = query.get(event.0) {
-            // spawn the dotnet process
-            let mut command = std::process::Command::new("wt");
-            command.args(
-                r"pwsh -NoProfile -c D:\Repos\Games\Cursor-Hero\other\start-hovershower.ps1"
-                    .split(' ')
-                    .map(|s| s.to_string())
-                    .collect(),
-            );
-            command.spawn().expect("failed to spawn process");
+    
+    // spawn the listener thread
 
-            // spawn the listener thread
+    let (tx, rx) = bounded::<u32>(10);
+    std::thread::spawn(move || {
+        let mut rng = StdRng::seed_from_u64(19878367467713);
+        loop {
+            // sleep for 1 second
+            std::thread::sleep(Duration::from_secs(1));
 
-            let (tx, rx) = bounded::<u32>(10);
-            std::thread::spawn(move || {
-                let mut rng = StdRng::seed_from_u64(19878367467713);
-                loop {
-                    // Everything here happens in another thread
-                    // This is where you could connect to an external data source
-                    let start_time = Instant::now();
-                    let duration = Duration::from_secs_f32(rng.gen_range(0.0..0.2));
-                    while start_time.elapsed() < duration {
-                        // Spinning for 'duration', simulating doing hard work!
-                    }
-
-                    tx.send(rng.gen_range(0..2000)).unwrap();
-                }
-            });
-
-            commands.insert_resource(StreamReceiver(rx));
+            tx.send(rng.gen_range(0..2000)).unwrap();
         }
-    }
+    });
+
+    commands.insert_resource(StreamReceiver(rx));
 }
 
-// This system reads from the receiver and sends events to Bevy
-fn read_stream(receiver: Res<StreamReceiver>, mut events: EventWriter<StreamEvent>) {
-    for from_stream in receiver.try_iter() {
-        events.send(StreamEvent(from_stream));
-    }
-}
+// fn handle_activation(
+//     mut commands: Commands,
+//     mut reader: EventReader<PressurePlateActivationEvent>,
+//     query: Query<&HoverShowerPressurePlate>,
+// ) {
+//     for event in reader.read() {
+//         if let Ok(plate) = query.get(event.0) {
+//             // spawn the dotnet process
+//             let mut command = std::process::Command::new("wt");
+//             command.args(
+//                 r"pwsh -NoProfile -c D:\Repos\Games\Cursor-Hero\other\start-hovershower.ps1"
+//                     .split(' ')
+//                     .map(|s| s.to_string())
+//                     .collect(),
+//             );
+//             command.spawn().expect("failed to spawn process");
+//         }
+//     }
+// }
 
-fn spawn_text(mut commands: Commands, mut reader: EventReader<StreamEvent>) {
-    let text_style = TextStyle {
-        font_size: 20.0,
-        color: Color::WHITE,
-        ..default()
-    };
+// // This system reads from the receiver and sends events to Bevy
+// fn read_stream(receiver: Res<StreamReceiver>, mut events: EventWriter<StreamEvent>) {
+//     for from_stream in receiver.try_iter() {
+//         events.send(StreamEvent(from_stream));
+//     }
+// }
 
-    for (per_frame, event) in reader.read().enumerate() {
-        commands.spawn((
-            Text2dBundle {
-                text: Text::from_section(event.0.to_string(), text_style.clone())
-                    .with_alignment(TextAlignment::Center),
-                transform: Transform::from_xyz(per_frame as f32 * 100.0, 300.0, 0.0),
-                ..default()
-            },
-            MyDebugText,
-        ));
-    }
-}
+// fn spawn_text(mut commands: Commands, mut reader: EventReader<StreamEvent>) {
+//     let text_style = TextStyle {
+//         font_size: 20.0,
+//         color: Color::WHITE,
+//         ..default()
+//     };
 
-fn move_text(
-    mut commands: Commands,
-    mut texts: Query<(Entity, &mut Transform), With<MyDebugText>>,
-    time: Res<Time>,
-) {
-    for (entity, mut position) in &mut texts {
-        position.translation -= Vec3::new(0.0, 100.0 * time.delta_seconds(), 0.0);
-        if position.translation.y < -300.0 {
-            commands.entity(entity).despawn();
-        }
-    }
-}
+//     for (per_frame, event) in reader.read().enumerate() {
+//         commands.spawn((
+//             Text2dBundle {
+//                 text: Text::from_section(event.0.to_string(), text_style.clone())
+//                     .with_alignment(TextAlignment::Center),
+//                 transform: Transform::from_xyz(per_frame as f32 * 100.0, 300.0, 0.0),
+//                 ..default()
+//             },
+//             MyDebugText,
+//         ));
+//     }
+// }
+
+// fn move_text(
+//     mut commands: Commands,
+//     mut texts: Query<(Entity, &mut Transform), With<MyDebugText>>,
+//     time: Res<Time>,
+// ) {
+//     for (entity, mut position) in &mut texts {
+//         position.translation -= Vec3::new(0.0, 100.0 * time.delta_seconds(), 0.0);
+//         if position.translation.y < -300.0 {
+//             commands.entity(entity).despawn();
+//         }
+//     }
+// }
