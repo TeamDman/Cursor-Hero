73e610ee37f4110bc549bdcdd00211857d6644a4
TeamDman <TeamDman9201@gmail.com>
2024-01-21 15:23:28 -0500

    disable when minimized, program still eating cpu tho

--- crates/screen/src/screen_update_plugin.rs
+++ crates/screen/src/screen_update_plugin.rs
@@ -5,6 +5,9 @@ use std::thread;
 use crate::screen_plugin::Screen;
 use bevy::prelude::*;
 use bevy::utils::HashMap;
+use bevy::window::PrimaryWindow;
+use bevy::window::RawHandleWrapper;
+use bevy::winit::WinitWindows;
 use cursor_hero_metrics::Metrics;
 use cursor_hero_winutils::win_screen_capture::get_full_monitor_capturers;
 use cursor_hero_winutils::win_screen_capture::MonitorRegionCapturer;
@@ -22,34 +25,37 @@ struct CapturedFrame {
 #[derive(Resource)]
 struct FrameHolderResource {
     frames: Arc<Mutex<HashMap<u32, CapturedFrame>>>,
+    enabled: Arc<Mutex<bool>>,
 }
 
 pub struct ScreenUpdatePlugin;
 impl Plugin for ScreenUpdatePlugin {
     fn build(&self, app: &mut App) {
         // Create a shared resource for captured frames
-        let captured_frames = Arc::new(Mutex::new(HashMap::new()));
+        let frames = Arc::new(Mutex::new(HashMap::new()));
 
         // Clone the Arc to move into the capture thread
-        let captured_frames_clone = Arc::clone(&captured_frames);
+        let frames_pointer = Arc::clone(&frames);
 
         let capturer_holder = Arc::new(Mutex::new(CapturerHolderResource {
             capturers: get_full_monitor_capturers().unwrap(),
         }));
 
-        let captured_frames = FrameHolderResource {
-            frames: captured_frames,
-        };
+        let enabled = Arc::new(Mutex::new(true));
+        let enabled_pointer = Arc::clone(&enabled);
+
+        let captured_frames = FrameHolderResource { frames, enabled };
 
         // Spawn a separate thread for capturing frames
         let ch = Arc::clone(&capturer_holder);
-        thread::spawn(move || {
-            loop {
-                // Capture logic here...
-                let frames = capture_frames(ch.clone());
-                let mut shared_frames = captured_frames_clone.lock().unwrap();
-                *shared_frames = frames;
+        thread::spawn(move || loop {
+            if !*enabled_pointer.lock().unwrap() {
+                thread::sleep(std::time::Duration::from_millis(100));
+                continue;
             }
+            let frames = capture_frames(ch.clone());
+            let mut shared_frames = frames_pointer.lock().unwrap();
+            *shared_frames = frames;
         });
 
         app.add_systems(Update, update_screens)
@@ -83,7 +89,20 @@ fn update_screens(
     mut textures: ResMut<Assets<Image>>,
     time: Res<Time>,
     frames: Res<FrameHolderResource>,
+    window_query: Query<(Entity, &RawHandleWrapper, &Window), With<PrimaryWindow>>,
+    winit_windows: NonSend<WinitWindows>,
 ) {
+    let (window_id, window_handle, window) = window_query.single();
+    let winit_window = winit_windows
+        .get_window(window_id)
+        .expect("Window not found");
+    if winit_window.is_minimized().unwrap_or(false) {
+        *frames.enabled.lock().unwrap() = false;
+        return;
+    } else {
+        *frames.enabled.lock().unwrap() = true;
+    }
+
     let monitor_frames = frames.frames.lock().unwrap();
     for (mut screen, texture) in &mut query {
         // tick the refresh rate timer
