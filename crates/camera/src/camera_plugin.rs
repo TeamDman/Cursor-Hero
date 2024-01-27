e588e1e9ef9bdb0055862245135c0afdcf012104
TeamDman <TeamDman9201@gmail.com>
2024-01-27 16:02:20 -0500

    Refactor camera plugin: Remove unused code and simplify input handling

--- crates/camera/src/camera_plugin.rs
+++ crates/camera/src/camera_plugin.rs
@@ -17,8 +17,7 @@ pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(InputManagerPlugin::<CameraAction>::default())
-            .add_systems(Startup, spawn_camera)
+        app.add_systems(Startup, spawn_camera)
             .add_systems(Update, (update_camera_zoom, handle_events))
             .add_event::<CameraEvent>()
             .add_systems(
@@ -34,28 +33,13 @@ impl Plugin for CameraPlugin {
 #[derive(Component, Reflect)]
 pub struct MainCamera;
 
-#[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug, Reflect)]
-pub enum CameraAction {
-    ToggleFollowCharacter,
-}
-
 fn spawn_camera(mut commands: Commands) {
-    let mut input_map = InputMap::default();
-    input_map.insert(KeyCode::Space, CameraAction::ToggleFollowCharacter);
-    input_map.insert(
-        InputKind::GamepadButton(GamepadButtonType::North),
-        CameraAction::ToggleFollowCharacter,
-    );
     commands.spawn((
         Camera2dBundle::default(),
         MainCamera,
         MovementDamping { factor: 0.90 },
         MassPropertiesBundle::new_computed(&Collider::ball(10.0), 1.0),
         RigidBody::Dynamic,
-        InputManagerBundle::<CameraAction> {
-            input_map,
-            action_state: ActionState::default(),
-        },
     ));
 }
 
