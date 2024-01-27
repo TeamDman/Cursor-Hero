d0b909640d346e3dc449ba9eaf44a2fa001f61d4
TeamDman <TeamDman9201@gmail.com>
2024-01-27 16:27:47 -0500

    Fix level bounds not using global transform

--- crates/level_bounds/src/level_bounds_plugin.rs
+++ crates/level_bounds/src/level_bounds_plugin.rs
@@ -14,7 +14,7 @@ pub struct LevelBoundsPlugin;
 impl Plugin for LevelBoundsPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<LevelBounds>()
-        .register_type::<LevelBoundsParent>()
+            .register_type::<LevelBoundsParent>()
             .add_event::<LevelBoundsEvent>()
             .add_systems(
                 Update,
@@ -127,7 +127,10 @@ pub fn handle_level_bounds_events(
                             ));
                         });
                 } else {
-                    debug!("Deferring level bounds event for environment {:?}", environment_id);
+                    debug!(
+                        "Deferring level bounds event for environment {:?}",
+                        environment_id
+                    );
                     new_deferred.push(event.clone());
                 }
             }
@@ -139,11 +142,11 @@ pub fn handle_level_bounds_events(
 #[allow(clippy::type_complexity)]
 fn enforce(
     mut character_query: Query<
-        (Entity, &Transform, &mut LinearVelocity),
+        (Entity, &GlobalTransform, &mut LinearVelocity),
         (With<Character>, Without<LevelBounds>),
     >,
     level_bounds: Query<
-        (&Transform, &Sprite, &CollidingEntities),
+        (&GlobalTransform, &CollidingEntities),
         (With<LevelBounds>, Without<Character>),
     >,
 ) {
@@ -151,27 +154,29 @@ fn enforce(
         character_query.iter_mut()
     {
         let mut is_in_bounds = false;
-        for bounds in level_bounds.iter() {
-            if bounds.2 .0.contains(&character_entity) {
+        for (_, in_bounds) in level_bounds.iter() {
+            if in_bounds.contains(&character_entity) {
                 is_in_bounds = true;
                 break;
             }
         }
         if !is_in_bounds {
+            // debug!("Found {} level bounds", level_bounds.iter().count());
             // apply a force to to the character in the direction of the nearest boundary
             let mut nearest_boundary = None;
             let mut nearest_boundary_distance = f32::MAX;
-            for bounds in level_bounds.iter() {
+            for (bounds_transform, _) in level_bounds.iter() {
                 let distance = character_transform
-                    .translation
-                    .distance(bounds.0.translation);
+                    .translation()
+                    .distance(bounds_transform.translation());
+                // debug!("Distance to boundary: {}", distance);
                 if distance < nearest_boundary_distance {
                     nearest_boundary_distance = distance;
-                    nearest_boundary = Some(bounds.0.translation);
+                    nearest_boundary = Some(bounds_transform.translation());
                 }
             }
             if let Some(nearest_boundary) = nearest_boundary {
-                let direction = nearest_boundary - character_transform.translation;
+                let direction = nearest_boundary - character_transform.translation();
                 character_velocity.0 +=
                     direction.normalize().xy() * direction.length_squared() / 1000.0;
             }
