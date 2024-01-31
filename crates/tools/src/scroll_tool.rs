f16578d22dbfd8d5d1b040858a20e75a9eb97b3a
TeamDman <TeamDman9201@gmail.com>
2024-01-30 23:45:06 -0500

    let sprint affect scroll

--- crates/tools/src/scroll_tool.rs
+++ crates/tools/src/scroll_tool.rs
@@ -1,8 +1,12 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::prelude::ReflectInspectorOptions;
 use bevy_inspector_egui::InspectorOptions;
+use cursor_hero_character::character_plugin::Character;
+use cursor_hero_math::Lerp;
+use cursor_hero_sprint_tool_types::sprint_tool_types_plugin::SprintEvent;
 use cursor_hero_toolbelt_types::prelude::*;
 use cursor_hero_winutils::win_mouse::scroll_wheel;
+use itertools::Itertools;
 use leafwing_input_manager::prelude::*;
 
 use crate::prelude::*;
@@ -13,7 +17,7 @@ impl Plugin for ScrollToolPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<ScrollTool>()
             .add_plugins(InputManagerPlugin::<ScrollToolAction>::default())
-            .add_systems(Update, (toolbelt_events, handle_input));
+            .add_systems(Update, (toolbelt_events, handle_input,handle_sprint_events));
     }
 }
 
@@ -32,7 +36,7 @@ impl Default for ScrollTool {
         Self {
             speed: 1.0,
             default_speed: 1.0,
-            sprint_speed: 4.0,
+            sprint_speed: 100.0,
         }
     }
 }
@@ -96,18 +100,65 @@ fn handle_input(
         let (tool_actions, tool) = zoom_tool;
         let mut amount = None;
         if tool_actions.pressed(ScrollToolAction::ScrollUp) {
+            if tool_actions.just_pressed(ScrollToolAction::ScrollUp) {
+                info!("Scrolling up");
+            }
             amount = Some(tool.speed * time.delta_seconds());
-            info!("Scrolling up");
         }
         if tool_actions.pressed(ScrollToolAction::ScrollDown) {
+            if tool_actions.just_pressed(ScrollToolAction::ScrollDown) {
+                info!("Scrolling down");
+            }
             amount = Some(-tool.speed * time.delta_seconds());
-            info!("Scrolling down");
         }
         if let Some(amount) = amount {
             match scroll_wheel(amount) {
                 Ok(_) => {}
                 Err(e) => {
                     error!("Error scrolling up: {:?}", e);
+                }
+            }
+        }
+    }
+}
+
+
+
+fn handle_sprint_events(
+    mut sprint_events: EventReader<SprintEvent>,
+    character_query: Query<&Children, With<Character>>,
+    toolbelt_query: Query<&Children, With<Toolbelt>>,
+    mut tool_query: Query<&mut ScrollTool>,
+) {
+    for event in sprint_events.read() {
+        let character_id = match event {
+            SprintEvent::Active { character_id, .. } => character_id,
+            SprintEvent::Stop { character_id } => character_id,
+        };
+        let Ok(character) = character_query.get(*character_id) else {
+            warn!("Character {:?} does not exist", character_id);
+            continue;
+        };
+        let character_kids = character;
+        let tool_ids = character_kids
+            .iter()
+            .filter_map(|kid| toolbelt_query.get(*kid).ok())
+            .flat_map(|toolbelt| toolbelt.iter())
+            .filter(|kid| tool_query.contains(**kid))
+            .cloned()
+            .collect_vec();
+
+        match event {
+            SprintEvent::Active { throttle, .. } => {
+                let mut iter = tool_query.iter_many_mut(&tool_ids);
+                while let Some(mut tool) = iter.fetch_next() {
+                    tool.speed = (tool.default_speed, tool.sprint_speed).lerp(*throttle);
+                }
+            }
+            SprintEvent::Stop { .. } => {
+                let mut iter = tool_query.iter_many_mut(&tool_ids);
+                while let Some(mut tool) = iter.fetch_next() {
+                    tool.speed = tool.default_speed;
                 }
             }
         }
