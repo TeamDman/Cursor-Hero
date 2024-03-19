f0e6edc9e2c099db0759e9ab0eb48da8139f0dda
TeamDman <TeamDman9201@gmail.com>
2024-03-18 23:48:19 -0400

    restore convert_hicon_to_rgba_image

--- crates/winutils/src/win_icons.rs
+++ crates/winutils/src/win_icons.rs
@@ -22,17 +22,14 @@ use windows::Win32::UI::WindowsAndMessaging::ICONINFOEXW;
 pub fn convert_hicon_to_rgba_image(hicon: &HICON) -> Result<RgbaImage> {
     unsafe {
         let mut icon_info = ICONINFOEXW::default();
-
-        // thank you valve https://github.com/ValveSoftware/wine/blob/941279cf95abce8c59ad350e6345734c9a75f0f2/dlls/winemac.drv/mouse.c#L775
-        icon_info.cbSize = std::mem::size_of::<ICONINFOEXW>() as u32;
-
+        icon_info.cbSize = std::mem::size_of::<ICONINFOEXW>() as u32; // thank you valve https://github.com/ValveSoftware/wine/blob/941279cf95abce8c59ad350e6345734c9a75f0f2/dlls/winemac.drv/mouse.c#L775
         GetIconInfoExW(*hicon, &mut icon_info).ok_with_description(format!(
             "icon_info := hicon • GetIconInfoExW: {} {}:{}",
             file!(),
             line!(),
             column!()
         ))?;
-        if icon_info.hbmColor.is_invalid() || icon_info.hbmMask.is_invalid() {
+        if icon_info.hbmColor.is_invalid() {
             return Err(Error::from_win32().with_description(format!(
                 "icon • GetIconInfoExW: {} {}:{}",
                 file!(),
@@ -41,12 +38,11 @@ pub fn convert_hicon_to_rgba_image(hicon: &HICON) -> Result<RgbaImage> {
             )));
         }
 
-        // Create the colour bitmap
-        let mut color_bmp = BITMAP::default();
+        let mut bmp = BITMAP::default();
         let bytes_stored = GetObjectW(
             icon_info.hbmColor,
             std::mem::size_of::<BITMAP>() as i32,
-            Some(&mut color_bmp as *mut _ as _),
+            Some(&mut bmp as *mut _ as _),
         );
         if bytes_stored == 0 {
             return Err(Error::from_win32().with_description(format!(
@@ -62,7 +58,7 @@ pub fn convert_hicon_to_rgba_image(hicon: &HICON) -> Result<RgbaImage> {
         let hdc_mem = CreateCompatibleDC(hdc_screen);
 
         // Create a compatible bitmap
-        let hbitmap = CreateCompatibleBitmap(hdc_screen, color_bmp.bmWidth, color_bmp.bmHeight);
+        let hbitmap = CreateCompatibleBitmap(hdc_screen, bmp.bmWidth, bmp.bmHeight);
         let hbm_old = SelectObject(hdc_mem, hbitmap);
 
         // Draw the icon onto the memory device context
@@ -71,8 +67,8 @@ pub fn convert_hicon_to_rgba_image(hicon: &HICON) -> Result<RgbaImage> {
             0,
             0,
             *hicon,
-            color_bmp.bmWidth,
-            color_bmp.bmHeight,
+            bmp.bmWidth,
+            bmp.bmHeight,
             0,
             None,
             DI_NORMAL,
@@ -84,11 +80,11 @@ pub fn convert_hicon_to_rgba_image(hicon: &HICON) -> Result<RgbaImage> {
             column!()
         ))?;
 
-        let mut color_bitmap_info = BITMAPINFO {
+        let mut bitmap_info = BITMAPINFO {
             bmiHeader: BITMAPINFOHEADER {
                 biSize: std::mem::size_of::<BITMAPINFOHEADER>() as u32,
-                biWidth: color_bmp.bmWidth,
-                biHeight: -color_bmp.bmHeight,
+                biWidth: bmp.bmWidth,
+                biHeight: bmp.bmHeight,
                 biPlanes: 1,
                 biBitCount: 32,
                 biCompression: DIB_RGB_COLORS.0,
@@ -98,15 +94,14 @@ pub fn convert_hicon_to_rgba_image(hicon: &HICON) -> Result<RgbaImage> {
         };
 
         // Allocate a buffer and get the bitmap bits
-        let mut color_buffer =
-            vec![0u8; color_bmp.bmWidth as usize * color_bmp.bmHeight as usize * 4];
+        let mut buffer = vec![0u8; bmp.bmWidth as usize * bmp.bmHeight as usize * 4];
         if GetDIBits(
             hdc_mem,
             hbitmap,
             0,
-            color_bmp.bmHeight as u32,
-            Some(color_buffer.as_mut_ptr() as *mut _),
-            &mut color_bitmap_info,
+            bmp.bmHeight as u32,
+            Some(buffer.as_mut_ptr() as *mut _),
+            &mut bitmap_info,
             DIB_RGB_COLORS,
         ) == 0
         {
@@ -118,79 +113,6 @@ pub fn convert_hicon_to_rgba_image(hicon: &HICON) -> Result<RgbaImage> {
             )));
         }
 
-        
-        // let mut mask_bmp = BITMAP::default();
-        // let bytes_stored = GetObjectW(
-        //     icon_info.hbmMask,
-        //     std::mem::size_of::<BITMAP>() as i32,
-        //     Some(&mut mask_bmp as *mut _ as _),
-        // );
-        // if bytes_stored == 0 {
-        //     return Err(Error::from_win32().with_description(format!(
-        //         "icon_info::hbmMask • GetObjectW: {} {}:{}",
-        //         file!(),
-        //         line!(),
-        //         column!()
-        //     )));
-        // }
-        
-        // let mut mask_bitmap_info = BITMAPINFO {
-        //     bmiHeader: BITMAPINFOHEADER {
-        //         biSize: std::mem::size_of::<BITMAPINFOHEADER>() as u32,
-        //         biWidth: mask_bmp.bmWidth,
-        //         biHeight: mask_bmp.bmHeight,
-        //         biPlanes: 1,
-        //         biBitCount: 32,
-        //         biCompression: DIB_RGB_COLORS.0,
-        //         ..Default::default()
-        //     },
-        //     bmiColors: [RGBQUAD::default(); 1],
-        // };
-        // let mut mask_buffer = vec![0u8; mask_bmp.bmWidth as usize * mask_bmp.bmHeight as usize * 1];
-        // if GetDIBits(
-        //     hdc_mem,
-        //     hbitmap,
-        //     0,
-        //     mask_bmp.bmHeight as u32,
-        //     Some(mask_buffer.as_mut_ptr() as *mut _),
-        //     &mut mask_bitmap_info,
-        //     DIB_RGB_COLORS,
-        // ) == 0
-        // {
-        //     return Err(Error::from_win32().with_description(format!(
-        //         "GetDIBits: {} {}:{}",
-        //         file!(),
-        //         line!(),
-        //         column!()
-        //     )));
-        // }
-
-        // Combine the color and mask bitmaps
-        let width = color_bmp.bmWidth as usize;
-        let height = color_bmp.bmHeight as usize;
-
-        let combined_buffer = color_buffer;
-        // let mut combined_buffer: Vec<u8> = Vec::with_capacity(width * height * 4);
-        // for y in 0..height {
-        //     for x in 0..width {
-        //         let color_index = (y * width + x) * 4;
-        //         let mask_index = y * width + x; // Assuming mask bitmap is 1 byte per pixel
-
-        //         let alpha = !mask_buffer[mask_index]; // Inverting the mask value since white is opaque and black is transparent
-
-        //         combined_buffer.push(color_buffer[color_index]); // Red
-        //         combined_buffer.push(color_buffer[color_index + 1]); // Green
-        //         combined_buffer.push(color_buffer[color_index + 2]); // Blue
-        //         combined_buffer.push(alpha); // Alpha
-        //     }
-        // }
-
-        // Create an image from the buffer
-        // This might be able to be done after cleanup since buffer is populated by then
-        let image = ImageBuffer::from_raw(width as u32, height as u32, combined_buffer)
-            .map(RgbaImage::from)
-            .ok_or_else(|| Error::ImageContainerNotBigEnough);
-
         // Clean up
         SelectObject(hdc_mem, hbm_old);
         DeleteObject(hbitmap).ok()?;
@@ -199,7 +121,10 @@ pub fn convert_hicon_to_rgba_image(hicon: &HICON) -> Result<RgbaImage> {
         DeleteObject(icon_info.hbmColor).ok()?;
         DeleteObject(icon_info.hbmMask).ok()?;
 
-        image
+        // Create an image from the buffer
+        ImageBuffer::from_raw(bmp.bmWidth as u32, bmp.bmHeight as u32, buffer)
+            .map(RgbaImage::from)
+            .ok_or_else(|| Error::ImageContainerNotBigEnough)
     }
 }
 
