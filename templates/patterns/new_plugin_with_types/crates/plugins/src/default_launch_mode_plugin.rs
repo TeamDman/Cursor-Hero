c6dc7753eeac67106b542c69f31b999d36815e2a
TeamDman <TeamDman9201@gmail.com>
2024-04-03 10:19:49 -0400

    trying to fix template

---
+++ templates/patterns/new_plugin_with_types/crates/plugins/src/default_launch_mode_plugin.rs
@@ -0,0 +1,7 @@
+{{use_statements}}
+use cursor_hero_{{crate_name}}::prelude::*;
+use cursor_hero_{{crate_name}}_types::prelude::*;
+{{plugin_start}}
+        app.add_plugins({{crate_name_pascal}}TypesPlugin);
+        app.add_plugins({{crate_name_pascal}}Plugin);
+        {{plugin_remaining}}
