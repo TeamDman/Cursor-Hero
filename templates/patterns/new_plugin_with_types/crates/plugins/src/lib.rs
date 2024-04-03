fb35731f874dadc1822161332e6654b688a1b1f8
TeamDman <TeamDman9201@gmail.com>
2024-04-03 10:20:00 -0400

    remove old injection point

--- templates/patterns/new_plugin_with_types/crates/plugins/src/lib.rs
+++
@@ -1,7 +0,0 @@
-{{use_statements}}
-use cursor_hero_{{crate_name}}::prelude::*;
-use cursor_hero_{{crate_name}}_types::prelude::*;
-{{plugin_start}}
-        app.add_plugins({{crate_name_pascal}}TypesPlugin);
-        app.add_plugins({{crate_name_pascal}}Plugin);
-        {{plugin_remaining}}
